2532030bba6c488e7dfc53859828ec70afcbe47d
Suliman Aljabali <sulimam.aljabali7@gmail.com>
2026-03-07 23:22:25 +0300

    Clean up the unused imports in BookService and BookController

--- src/main/java/com/books/demo/BookController.java
+++ src/main/java/com/books/demo/BookController.java
@@ -2,14 +2,11 @@ package com.books.demo;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping(path = "/api/v1")
--- src/main/java/com/books/demo/BookService.java
+++ src/main/java/com/books/demo/BookService.java
@@ -1,10 +1,8 @@
 package com.books.demo;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
+
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
 
